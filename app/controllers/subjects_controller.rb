dd294513c16c8d2b4abc19c6cec5e84cf16ec40f
kujenga <ataylor0123@gmail.com>
2014-06-11 00:24:53 -0400

    added CRUD handling from lynda tutorial

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -7,14 +7,57 @@ class SubjectsController < ApplicationController
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new(:name => "Default")
+  end
+
+  def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+      # is save suceeds, redirect to the index action
+      flash[:notice] = "Subject created sucessfully!"
+      redirect_to(:action => 'index')
+    else
+      # if save fails, redisplay the form so user can fix problems
+      render('new')
+    end
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    # Find an existing object using form parameters
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+      # is save suceeds, redirect to the index action
+      flash[:notice] = "Subject updated sucessfully!"
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      # if save fails, redisplay the form so user can fix problems
+      render('new')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject '#{subject.name}' destroyed sucessfully."
+    redirect_to(:action => 'index')
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
   end
 end
